3f3defd295c3948d1266585c55cfa6d1d1d90b3f
theborakompanioni <theborakompanioni+github@gmail.com>
2016-10-03 23:47:24 +0200

    add EMptyMetricsFilterTest

--- src/main/java/io/github/tbk/postrics/metrics/EmptyMetricsFilter.java
+++ src/main/java/io/github/tbk/postrics/metrics/EmptyMetricsFilter.java
@@ -3,14 +3,10 @@ package io.github.tbk.postrics.metrics;
 import com.codahale.metrics.Gauge;
 import com.codahale.metrics.Metric;
 import com.codahale.metrics.MetricFilter;
-import com.codahale.metrics.RatioGauge;
 
 public final class EmptyMetricsFilter implements MetricFilter {
     @Override
     public boolean matches(final String name, final Metric metric) {
-        if (metric instanceof RatioGauge) {
-            return isValidDouble(((RatioGauge) metric).getValue());
-        }
         if (metric instanceof Gauge) {
             final Object val = ((Gauge) metric).getValue();
             if (val instanceof Double) {
---
+++ src/test/java/io/github/tbk/postrics/metrics/EmptyMetricsFilterTest.java
@@ -0,0 +1,30 @@
+package io.github.tbk.postrics.metrics;
+
+import com.codahale.metrics.Gauge;
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.hamcrest.Matchers.is;
+import static org.junit.Assert.assertThat;
+
+public class EmptyMetricsFilterTest {
+
+    private EmptyMetricsFilter sut;
+
+    @Before
+    public void setUp() {
+        this.sut = new EmptyMetricsFilter();
+    }
+
+    @Test
+    public void itShouldFilterDoubleValueNan() throws Exception {
+        final boolean matches = this.sut.matches("test", (Gauge<Double>) () -> Double.NaN);
+        assertThat(matches, is(false));
+    }
+
+    @Test
+    public void itShouldNotFilterDoubleValueZero() throws Exception {
+        final boolean matches = this.sut.matches("test", (Gauge<Double>) () -> 0d);
+        assertThat(matches, is(true));
+    }
+}
